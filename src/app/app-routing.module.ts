baff5235e23287a94d077f3d14b0a26ce35a677f
Milad Tahmasebi <tahmasebimilad101@gmail.com>
2022-03-12 13:46:16 +0330

    faq

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FaqComponent } from './component/faq/faq.component';
 import { SearchComponent } from './component/search/search.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
   {path:'' , component: HomePageComponent},
-  {path:'' ,  component: SearchComponent}
+  {path:'' ,  component: SearchComponent},
+  {path:'faq-page' ,  component: FaqComponent},
 ];
 
 @NgModule({
--- src/app/component/faq/faq.component.ts
+++ src/app/component/faq/faq.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IFaq } from 'src/app/model/faq-model';
+import { ApiUrlService } from 'src/app/servis/api-url.service';
 
 @Component({
   selector: 'app-faq',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FaqComponent implements OnInit {
 
-  constructor() { }
+  info: any;
+  faq: IFaq;
+  expandedIndex = 0;
+  constructor(
+    public apiurl: ApiUrlService,
+    private http: HttpClient,
+  ) { }
 
   ngOnInit(): void {
+    this.openquestion();
+  }
+  onTitleClick(btnVal: any) {
+    this.faq.Content = btnVal.description;
+  }
+  openquestion() {
+
+    this.apiurl.getFaq().subscribe((arg: any) => {
+      this.faq = arg.data;
+      console.log("inooooo222", this.faq)
+    }, (err) => {
+      console.log(err);
+
+    });
   }
 
 }
